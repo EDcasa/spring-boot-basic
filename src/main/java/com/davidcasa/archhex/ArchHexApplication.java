02cb95c813886151338ce8906ab77a1c5f120bd4
EDcasa <edwin.casa01@hotmail.com>
2024-11-08 22:46:49 -0500

    [profiles], configuration to profiles yml properties

---
+++ src/main/java/com/davidcasa/archhex/ArchHexApplication.java
@@ -0,0 +1,13 @@
+package com.davidcasa.archhex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArchHexApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ArchHexApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/davidcasa/archhex/ArchHexApplicationTests.java
@@ -0,0 +1,13 @@
+package com.davidcasa.archhex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArchHexApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
